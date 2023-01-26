2c7effc282cdad46d2bf76c27de670d0db1c5e98
Brett Chalupa <brettchalupa@gmail.com>
2023-01-26 10:58:52 -0500

    chore: remove unused text key

--- app/text.rb
+++ app/text.rb
@@ -8,7 +8,6 @@ TEXT = {
   controls_title: "Controls",
   controls_keyboard: "WASD/Arrows to move | J/Z/Space to confirm | Esc/P to pause",
   controls_gamepad: "Stick/D-Pad to move | A to confirm | Start to pause",
-  exp_to_next_level: "Exp to Next Level",
   fullscreen: "Fullscreen",
   made_by: "A game by",
   off: "OFF",
